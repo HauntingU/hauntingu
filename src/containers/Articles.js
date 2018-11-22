3806ca287bb90bc68734a5377738dd2ae1c921b3
Ryan Festag <rfestag@gmail.com>
2018-11-22 08:14:25 -0700

    Fixed links for articles

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Redirect } from 'react-router';
 import { withRouteData, Link } from 'react-static'
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
@@ -12,46 +13,61 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography'
 import ArticleEntry from '../components/ArticleEntry'
 
-export default withRouteData(({ articles }) => (
-  <div>
-    <Grid item xs={12}>
-      <Grid container justify="center">
-        <Grid item xs={12} md={8} lg={6}>
-          <Typography variant="h4" gutterBottom>
-            Articles
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid container justify="center">
-        <Grid item xs={12} md={8} lg={6}>
+class Articles extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  handleOnClick = (slug) => {
+    this.setState({redirect: `/articles/article/${slug}`});
+  }
+  render() {
+    const {redirect} = this.state
+    const {articles} = this.props
+
+    return redirect ? (<Redirect push to={redirect} />) : (
+      <div>
+        <Grid item xs={12}>
           <Grid container justify="center">
-            <List style={{display: 'flex', alignItems: 'stretch', flexDirection: 'column'}}>
-              {articles.map(article => (
-                [
-                  (<ListItem button key="item" onClick={() => this.handleOnClick(article.slug)} style={{minHeight: 100, maxHeight: 200, display: 'flex'}}>
-                    <CardMedia
-                      style={{width: 151, height: '100%'}}
-                      image={article.image}
-                      title={article.title}
-                    />
-                    <ListItemText
-                      primary={article.title}
-                      secondary={article.summary}
-                      style={{flex: 1, maxHeight: 200, overflow: 'hidden'}}
-                    />
-                  </ListItem>),
-                  (<Divider key="divider" inset={true} component="li" />)
-                ]
-              ))}
-            </List>
-            {/*articles.map(article => (
-              <Grid item xs={12} key={article.slug}>
-                <ArticleEntry article={article} />
+            <Grid item xs={12} md={8} lg={6}>
+              <Typography variant="h4" gutterBottom>
+                Articles
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container justify="center">
+            <Grid item xs={12} md={8} lg={6}>
+              <Grid container justify="center">
+                <List style={{display: 'flex', alignItems: 'stretch', flexDirection: 'column'}}>
+                  {articles.map(article => (
+                    [
+                      (<ListItem button key="item" onClick={() => this.handleOnClick(article.slug)} style={{minHeight: 100, maxHeight: 200, display: 'flex'}}>
+                        <CardMedia
+                          style={{width: 151, height: '100%'}}
+                          image={article.image}
+                          title={article.title}
+                        />
+                        <ListItemText
+                          primary={article.title}
+                          secondary={article.summary}
+                          style={{flex: 1, maxHeight: 200, overflow: 'hidden'}}
+                        />
+                      </ListItem>),
+                      (<Divider key="divider" inset={true} component="li" />)
+                    ]
+                  ))}
+                </List>
+                {/*articles.map(article => (
+                  <Grid item xs={12} key={article.slug}>
+                    <ArticleEntry article={article} />
+                  </Grid>
+                ))*/}
               </Grid>
-            ))*/}
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-  </div>
-))
+      </div>
+    )
+  }
+}
+export default withRouteData(Articles)
